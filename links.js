58b49bca60a891cf0f2a3c8f2546893029164577
mutmedia <gustavo.ceci95@gmail.com>
2022-02-13 01:17:10 -0500

    the webpage

---
+++ links.js
@@ -0,0 +1,8 @@
+export default {
+  "mut/moochi": "https://mut.media",
+  "dumpling.love":	"dumpling.love",
+  "paradise.fm": 	"https://paradise-place.github.io/paradise.fm/",
+  "Mer Grazzini":	"https://mergrazzini.itch.io",
+  "kya 🌱": "https://withkya.com",
+  "Mokesmoe":	"https://mokesmoe.com/"
+}
