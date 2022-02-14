43dfe3ac9225d542c85773e55fe2688a5b346568
mut / moochi <gustavo.ceci95@gmail.com>
2022-02-14 12:10:19 -0500

    gate, q1, ash k and Erg
    and updated thie links properly

--- links.js
+++ links.js
@@ -1,10 +1,14 @@
 export default {
   "mut/moochi": "https://mut.media",
-  "dumpling.love":	"dumpling.love",
+  "dumpling.love":	"https://dumpling.love",
   "paradise.fm": 	"https://paradise-place.github.io/paradise.fm/",
   "Mer Grazzini":	"https://mergrazzini.itch.io",
   "kya 🌱": "https://withkya.com",
   "Mokesmoe":	"https://mokesmoe.com/",
-  "brin": "brin.neocities.org",
-  "amber":	"https://www.fiddlemouth.gay/"
+  "brin": "https://brin.neocities.org",
+  "Amber Hope / fiddlemouth":	"https://www.fiddlemouth.gay/",
+  "gate": "https://gate.itch.io/",
+  "q1": "https://quewon.github.io/",
+  "ash k": "https://ash-k.dev/",
+  "Erg": "https://ergman.itch.io/",
 }
